24793f88891173a7e1fc5032058ddc64aa862344
Andranik Muradyan <muradianator@gmail.com>
2018-09-09 00:34:36 +0400

    Solved the 'Even Fibonacci numbers' problem

--- ProjectEuler.pas
+++ ProjectEuler.pas
@@ -1,6 +1,29 @@
 program ProjectEuler;
 
-procedure SumOfMultiplesOf3And5;
+procedure EvenFibonacciNumbers;
+
+var
+  fibPrev: Integer;
+  fibCurr: Integer;
+  sum: Integer;
+begin
+  fibPrev := 1;
+  fibCurr := 2;
+  sum := 0;
+
+  While fibCurr < 4000001 do
+    begin
+      If (fibCurr mod 2 = 0) then sum := sum + fibCurr;
+
+      fibCurr := fibCurr + fibPrev;
+      fibPrev := fibCurr - fibPrev;
+    end;
+
+  Write('Even Fibonacci Numbers - ');
+  WriteLn(sum);
+end;
+
+procedure MultiplesOf3And5;
 
 var
   i: Integer;
@@ -10,18 +33,16 @@ begin
 
   For i := 0 to 999 do
   begin
-    If (i mod 3 = 0) or (i mod 5 = 0) then
-    begin
-       WriteLn(i);
-       sum := sum + i;
-    end;
+    If (i mod 3 = 0) or (i mod 5 = 0) then sum := sum + i;
   end;
 
+  Write('Sum Of Multiples Of 3 And 5 - ');
   WriteLn(sum);
-  ReadLn;
 end;
 
 begin
-  SumOfMultiplesOf3And5();
+  MultiplesOf3And5;
+  EvenFibonacciNumbers;
+  ReadLn;
 end.
 
