131532b273b8265d0add38c56f1685b19eb4828c
Andranik Muradyan <andranik.muradyan@scdm.de>
2018-09-11 00:55:44 +0400

    Artak6

--- install.sh
+++ install.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
 sudo apt-get update
-echo | gcc -v
-echo | ld -v
-
 wget -O lazarus-project_1.8.4-0_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/lazarus-project_1.8.4-0_amd64.deb/download
 # wget -O fpc_3.0.4-3_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc_3.0.4-3_amd64.deb/download
 wget -O fpc-3.0.4.x86_64-linux.tar https://sourceforge.net/projects/freepascal/files/Linux/3.0.4/fpc-3.0.4.x86_64-linux.tar/download
 # wget -O fpc-src_3.0.4-2_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc-src_3.0.4-2_amd64.deb/download
 tar -xvf fpc-3.0.4.x86_64-linux.tar
-pushd fpc-3.0.4.x86_64-linux
 
+pushd fpc-3.0.4.x86_64-linux
 echo 'echo /usr' > answers.sh
 echo 'echo n' >> answers.sh
 echo 'echo n' >> answers.sh
 echo 'echo n' >> answers.sh
-
-chmod 777 answers.sh
-
+chmod +x answers.sh
 ./answers.sh | sudo ./install.sh
-
 popd
+
 sudo dpkg -i lazarus-project_1.8.4-0_amd64.deb > /dev/null
-sudo apt-get -f install --fix-missing
+# sudo apt-get -f install --fix-missing
 git clone https://github.com/graemeg/fptest
+
 pushd fptest
 lazbuild fptest.lpk
 popd
