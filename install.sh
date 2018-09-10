ab303acc9d26519913dbc2cb4158004236478706
Andranik Muradyan <andranik.muradyan@scdm.de>
2018-09-11 00:38:34 +0400

    FPC from tarball with answers and sudo

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ wget -O lazarus-project_1.8.4-0_amd64.deb https://sourceforge.net/projects/lazar
 wget -O fpc-3.0.4.x86_64-linux.tar https://sourceforge.net/projects/freepascal/files/Linux/3.0.4/fpc-3.0.4.x86_64-linux.tar/download
 # wget -O fpc-src_3.0.4-2_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc-src_3.0.4-2_amd64.deb/download
 tar -xvf fpc-3.0.4.x86_64-linux.tar
-cd fpc-3.0.4.x86_64-linux
+pushd fpc-3.0.4.x86_64-linux
 
 echo 'echo /usr' > answers.sh
 echo 'echo n' >> answers.sh
@@ -19,6 +19,8 @@ echo 'echo n' >> answers.sh
 chmod 777 answers.sh
 
 ./answers.sh | sudo ./install.sh
+
+popd
 
 sudo dpkg -i lazarus-project_1.8.4-0_amd64.deb > /dev/null
 git clone https://github.com/graemeg/fptest
