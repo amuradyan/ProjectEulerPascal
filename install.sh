ddd612c590f6c4daeaa0add833f7ae338f8652db
Andranik Muradyan <andranik.muradyan@scdm.de>
2018-09-11 00:31:48 +0400

    FPC from tarball with answers and sudo

--- install.sh
+++ install.sh
@@ -5,23 +5,22 @@ echo | gcc -v
 echo | ld -v
 
 wget -O lazarus-project_1.8.4-0_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/lazarus-project_1.8.4-0_amd64.deb/download
-wget -O fpc_3.0.4-3_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc_3.0.4-3_amd64.deb/download
-# wget -O fpc-3.0.4.x86_64-linux.tar https://sourceforge.net/projects/freepascal/files/Linux/3.0.4/fpc-3.0.4.x86_64-linux.tar/download
-wget -O fpc-src_3.0.4-2_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc-src_3.0.4-2_amd64.deb/download
-# tar -xvf fpc-3.0.4.x86_64-linux.tar
-# cd fpc-3.0.4.x86_64-linux
+# wget -O fpc_3.0.4-3_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc_3.0.4-3_amd64.deb/download
+wget -O fpc-3.0.4.x86_64-linux.tar https://sourceforge.net/projects/freepascal/files/Linux/3.0.4/fpc-3.0.4.x86_64-linux.tar/download
+# wget -O fpc-src_3.0.4-2_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc-src_3.0.4-2_amd64.deb/download
+tar -xvf fpc-3.0.4.x86_64-linux.tar
+cd fpc-3.0.4.x86_64-linux
 
-# echo 'echo /usr' > answers.sh
-# echo 'echo n' >> answers.sh
-# echo 'echo n' >> answers.sh
-# echo 'echo n' >> answers.sh
+echo 'echo /usr' > answers.sh
+echo 'echo n' >> answers.sh
+echo 'echo n' >> answers.sh
+echo 'echo n' >> answers.sh
 
-# chmod 777 answers.sh
+chmod 777 answers.sh
 
-# ./answers.sh | sudo ./install.sh
+./answers.sh | sudo ./install.sh
 
-sudo dpkg -i lazarus-project_1.8.4-0_amd64.deb fpc-src_3.0.4-2_amd64.deb fpc_3.0.4-3_amd64.deb > /dev/null
-sudo apt-get -f install --fix-missing
+sudo dpkg -i lazarus-project_1.8.4-0_amd64.deb > /dev/null
 git clone https://github.com/graemeg/fptest
 cd fptest
 lazbuild fptest.lpk
