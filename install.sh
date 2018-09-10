688e2e8ba3b195f1381f7506e59388e7fe601c76
Andranik Muradyan <andranik.muradyan@scdm.de>
2018-09-10 23:36:41 +0400

    FPC from tarball with answers and sudo

--- install.sh
+++ install.sh
@@ -15,7 +15,10 @@ echo 'echo n' >> answers.sh
 echo 'echo n' >> answers.sh
 echo 'echo n' >> answers.sh
 
-sudo ./install.sh
+chmod +x answers.sh
+
+sudo ./answers.sh | ./install.sh
+
 # wget -O fpc_3.0.4-3_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc_3.0.4-3_amd64.deb/download
 sudo dpkg -i lazarus-project_1.8.4-0_amd64.deb fpc-src_3.0.4-2_amd64.deb > /dev/null
 sudo apt-get -f install --fix-missing
