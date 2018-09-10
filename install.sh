862723fa2dfa85c0abbf8ea7f1792fc4ed3ee480
Andranik Muradyan <andranik.muradyan@scdm.de>
2018-09-10 22:42:50 +0400

    FPC from tarball

--- install.sh
+++ install.sh
@@ -5,9 +5,13 @@ echo | gcc -v
 echo | ld -v
 
 wget -O lazarus-project_1.8.4-0_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/lazarus-project_1.8.4-0_amd64.deb/download
-wget -O fpc_3.0.4-3_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc_3.0.4-3_amd64.deb/download
+wget -O fpc-3.0.4.x86_64-linux.tar https://sourceforge.net/projects/freepascal/files/Linux/3.0.4/fpc-3.0.4.x86_64-linux.tar/download
 wget -O fpc-src_3.0.4-2_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc-src_3.0.4-2_amd64.deb/download
-sudo dpkg -i lazarus-project_1.8.4-0_amd64.deb fpc_3.0.4-3_amd64.deb fpc-src_3.0.4-2_amd64.deb > /dev/null
+tar -xvf fpc-3.0.4.x86_64-linux.tar
+cd fpc-3.0.4.x86_64-linux
+./install.sh
+# wget -O fpc_3.0.4-3_amd64.deb https://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc_3.0.4-3_amd64.deb/download
+sudo dpkg -i lazarus-project_1.8.4-0_amd64.deb fpc-src_3.0.4-2_amd64.deb > /dev/null
 sudo apt-get -f install --fix-missing
 git clone https://github.com/graemeg/fptest
 cd fptest
